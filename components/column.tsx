1de0c95962588a3d7d3652d8d114344ec4544c7c
Ndzishepngong George Tarla <128661655+GeorgeeFlash@users.noreply.github.com>
2025-05-19 10:53:32 +0100

    Update column.tsx
    height for mobile

--- components/column.tsx
+++ components/column.tsx
@@ -33,27 +33,27 @@ export default function Column({
   }
 
   return (
-    <section className='min-h-[250px] md:min-h-[600px]'>
+    <section className='flex flex-col min-h-[250px] md:min-h-[600px]'>
       <h2 className='ml-1 font-serif text-2xl font-semibold'>{title}</h2>
 
       <div
-        className='mt-3.5 h-full w-full rounded-xl bg-gray-700/50 p-4'
+        className='flex-1 mt-3.5 h-full w-full rounded-xl bg-gray-700/50 p-4'
         onDrop={handleDrop}
         onDragOver={e => e.preventDefault()}
       >
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col h-full gap-4'>
           {filteredTasks.map(task => (
             <Task key={task.id} {...task} />
           ))}
 
           {filteredTasks.length === 0 && status === 'TODO' && (
-            <div className='mt-8 text-center text-sm text-gray-500'>
+            <div className='h-full mt-8 text-center text-sm text-gray-500'>
               <p>Create a new task</p>
             </div>
           )}
 
           {tasks.length && filteredTasks.length === 0 && status !== 'TODO' ? (
-            <div className='mt-8 text-center text-sm text-gray-500'>
+            <div className='h-full mt-8 text-center text-sm text-gray-500'>
               <p>Drag your tasks here</p>
             </div>
           ) : null}
